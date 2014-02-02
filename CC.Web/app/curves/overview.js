589a0cd27ce12e37340f0e0f0d21cae0c1ccbd0f
Brian Keating <b@briankeating.net>
2014-02-02 15:39:06 +0000

    initial commit

---
+++ CC.Web/app/curves/overview.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'overview';
+    angular.module('app').controller(controllerId, ['common', 'datacontext', overview]);
+
+    function overview(common, datacontext) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        vm.news = {
+            title: 'Genic Insight',
+            description: 'Datagenic Dashboard.'
+        };
+        vm.messageCount = 0;
+        vm.people = [];
+        vm.title = 'Overview';
+
+        activate();
+
+        function activate() {
+            var promises = [getMessageCount(), getPeople()];
+            common.activateController(promises, controllerId)
+                .then(function () { log('Activated overview View'); });
+        }
+
+        function getMessageCount() {
+            return datacontext.getMessageCount().then(function (data) {
+                return vm.messageCount = data;
+            });
+        }
+
+        function getPeople() {
+            return datacontext.getPeople().then(function (data) {
+                return vm.people = data;
+            });
+        }
+    }
+})();
---
+++ CC.Web/app/services/datacontext.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+
+    var serviceId = 'datacontext';
+    angular.module('app').factory(serviceId, ['common', datacontext]);
+
+    function datacontext(common) {
+        var $q = common.$q;
+
+        var service = {
+            getPeople: getPeople,
+            getMessageCount: getMessageCount
+        };
+
+        return service;
+
+        function getMessageCount() { return $q.when(72); }
+
+        function getPeople() {
+            var people = [
+                { firstName: 'Brian', lastName: 'Keating', age: 25, location: 'Ireland' },
+                { firstName: 'Colin', lastName: 'Hartley', age: 31, location: 'Essex' },
+                { firstName: 'Angular', lastName: 'DotJs', age: 35, location: 'Everywhere' }
+            ];
+            return $q.when(people);
+        }
+    }
+})();
